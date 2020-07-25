abe512768dfc1f77b5f304adf1281239be31e565
Renato Tramontano <r.tramontano@reply.it>
2020-07-25 23:34:11 +0200

    update post and get

--- LibrarySDK/APIs/APIs.swift
+++ LibrarySDK/APIs/APIs.swift
@@ -1,46 +1,42 @@
 import UIKit
+import Combine
 
 class APIManager {
-    static let baseURL = "http://kingborn187.altervista.org/BibliotecaREST/libro/read.php"
-    static let baseURL2 = "http://kingborn187.altervista.org/BibliotecaREST/libro/create.php"
+    static let baseURL = "http://kingborn187.altervista.org/BibliotecaREST/libro"
+    static let readURL = URL(string: baseURL + "/read.php")!
+    static let createURL = URL(string: baseURL + "/create.php")!
 }
 
 public class BooksApi {
+    public var cancellable: AnyCancellable?
+
     public init() {}
 
-    public func getBooks() -> Record? {
-        let url = APIManager.baseURL
-        var record: Record?
-        let task = URLSession.shared.dataTask(with: URL(string: url)!) { data, response, error in
-            guard let data = data else { return }
-            do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                record = try decoder.decode(Record.self, from: data)
-            } catch let error {
-                print(error.localizedDescription)
-            }
-        }
-        task.resume()
-        return record
+    public func getNewBooks() -> AnyPublisher<LibrarySDK.Record, Error> {
+        URLSession.shared.dataTaskPublisher(for: APIManager.readURL)
+            .map { $0.data }
+            .decode(type: LibrarySDK.Record.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
     }
 
     public func setBooks(isbn: String, autore: String, titolo: String) {
         let book = Libri(ISBN: isbn, autore: autore, titolo: titolo)
-
+        var urlReqest = URLRequest(url: APIManager.createURL)
+        urlReqest.httpMethod = "POST"
         do {
-            let jsonData = try JSONEncoder().encode(book)
-            var request = URLRequest(url: URL(string: APIManager.baseURL2)!)
-            request.httpMethod = "POST"
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-
-            let task = URLSession.shared.uploadTask(with: request, from: jsonData) { data, response, error in
-                guard let data = data else { return }
-                print(String(data: data, encoding: .utf8)!)
-            }
-
-            task.resume()
+            urlReqest.httpBody = try JSONEncoder().encode(book)
         } catch {}
+        urlReqest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        cancellable = URLSession.shared.dataTaskPublisher(for: urlReqest)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { (completion) in
+                switch completion {
+                    case .failure(let error): print(error)
+                    case .finished: print("GET - DONE")
+                }
+            }, receiveValue: { response in
+                print(response)
+            })
     }
 }
 
